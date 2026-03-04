24e1ed2b70d8e7df9aef37df5d2b3b43c77e061d
hangu26 <125523867+hangu26@users.noreply.github.com>
2026-03-04 17:46:27 +0900

    refactor : UrlFramgent 태그 onItemMove 함수 변경(selectedPosition 보정 로직 추가) #5

--- app/src/main/java/kr/baeksuk/urlbox/util/adapter/RvTagAdapter.kt
+++ app/src/main/java/kr/baeksuk/urlbox/util/adapter/RvTagAdapter.kt
@@ -119,7 +119,25 @@ class RvTagAdapter(
         Collections.swap(updatedList, from, to)
         tagList = updatedList
 
+        // 1. 선택 위치(selectedPosition) 정보 갱신
+        val oldSelectedPosition = selectedPosition
+        if (selectedPosition == from) {
+            selectedPosition = to
+        } else if (from < to && selectedPosition > from && selectedPosition <= to) {
+            selectedPosition--
+        } else if (from > to && selectedPosition < from && selectedPosition >= to) {
+            selectedPosition++
+        }
+
         notifyItemMoved(from, to)
+        notifyItemChanged(from)
+        notifyItemChanged(to)
+
+        // 만약 이동 중에 선택된 아이템의 하이라이트가 깨진다면 아래 코드도 추가
+        if (oldSelectedPosition != selectedPosition) {
+            notifyItemChanged(oldSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
     }
 
 
